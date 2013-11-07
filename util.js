cb02baeb4c50e899ce55a319ca20ac0df054a25f
John DeHerrera <jdeherrera@gmail.com>
2013-11-07 09:04:24 -0600

    Added guid function

--- util.js
+++ util.js
@@ -14,5 +14,13 @@
 		}
 		
 		return obj || defaultValue || '';
-	});	
+	});
+	
+	global.util.guid = function() {
+	        var S4 = function() {
+	            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+	        };
+	
+	        return (S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4());
+    	}
 {)(this);
